1c6af78d7df252fd84bb4d6c657b6d6229b5e15b
joaquin <joacoarribas@hotmail.com>
2015-09-19 11:22:57 -0300

    Terminado ejercicio 1

--- ej1/ej1.cpp
+++ ej1/ej1.cpp
@@ -10,14 +10,19 @@
 #include <sys/time.h>
 
 
-int ej1(std::vector<int> conexiones, std::vector<std::vector<int> > pisos) {
+int ejUno(std::vector<int> conexiones, std::vector<std::vector<int> > pisos) {
   int n = conexiones.size();
-  int max;
+  int max = 0;
   for (int i = 1 ; i < n ; i++) {
-    for (int j = 0 ; j < i - 1 ; j++) {
-      
+    for (int j = 0 ; j < i ; j++) {
+      if (pisos[j][i] == 1) {
+        if (max < conexiones[j])
+          max = conexiones[j];
+      }
+      conexiones[i] = max + 1;
     }
   }
+  return conexiones[n-1];
 }
 
 int evaluarTests(std::string fileTestData, std::string fileTestResult, std::string fileTestWrite) {
@@ -27,6 +32,7 @@ int evaluarTests(std::string fileTestData, std::string fileTestResult, std::stri
   std::ofstream fileWrite (fileTestWrite.c_str());
   std::string s;
   std::string res;
+  int i = 1;
   // Abri los archivos de datos y resultados
   // e instancie las variables necesarias para el problema
   // el vector de exploradoras y el vector de amistades 
@@ -57,10 +63,11 @@ int evaluarTests(std::string fileTestData, std::string fileTestResult, std::stri
       int j;
       is >> j;
       pisos[j][i] = 1;
+      pisos[i][j] = 1;
     }
 
 
-//    int res = ejUno(conexiones, pisos);
+    int res = ejUno(conexiones, pisos);
     std::cout << "Matriz adyacencia: " << std::endl;
     for (int i = 0 ; i < cant_pisos ; i++) {
       for (int j = 0 ; j < cant_pisos ; j++) {
@@ -69,8 +76,25 @@ int evaluarTests(std::string fileTestData, std::string fileTestResult, std::stri
       std::cout << std::endl;
     }
 
-  }
+    getline (fileResult, line);
 
+    // Lei una linea del archivo de resultados
+    // y pregunto si ya termine de evaluar todos los tests
+
+    int resTest = atoi(line.c_str());
+    // convierto a int
+
+    if (res == resTest) {
+      std::cout << "Paso el test " << i << ". Felicitaciones!" << std::endl;
+    } else {
+      std::cout << "Fallo el test " << i << ". :(" << std::endl;
+      std::cout << "Obtuve " << res << " deberia tener " << resTest << std::endl;
+    }
+
+    i++;
+
+  }
+  return 0;
 }
 
 int main(int argc, char** argv) {
