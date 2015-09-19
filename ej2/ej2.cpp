a2b174313effbb181badfa0392fc634b32f0b5b2
cosa65 <mfosc65@gmail.com>
2015-09-19 18:59:17 -0300

    class e in/out del ejercicio 2

--- ej2/ej2.cpp
+++ ej2/ej2.cpp
@@ -1,86 +1,30 @@
-#include <iostream>
-#include <sstream>
-#include <istream>
-#include <fstream>
-#include <list>
-#include <vector>
-
-using namespace std;
-
-struct arista{
-	int piso;
-	int pos;
-	int tiempo;
-};
-
-struct nodo{
-	list<arista> aristas;
-	bool activo;									//Si no es un nodo o si sí lo es
-	void insertarArista(int piso, int pos, int peso);
-};
-
-void nodo::insertarArista(int piso, int pos, int peso){
-	arista nueva;
-	nueva.piso = piso;
-	nueva.pos = pos;
-	nueva.tiempo = peso;
-	aristas.push_back(nueva);
-}
-
-typedef vector < vector< nodo > > grafo;	// Identifico a los nodos por dos valores, piso (filas de la matriz) y metros (columnas de la matriz)
-
-int masCercanoIzq(grafo pabellon, int piso, int pos){
-	for(int i=pos-1; i>=0; i--){
-		if(pabellon[piso][i].activo){
-			return i;
-		}
-	}
-	return -1;
-}
-
-int masCercanoDer(grafo pabellon, int piso, int pos){
-	int L = pabellon[piso].size();
-	for(int i=pos+1; i<L; i++){
-		if(pabellon[piso][i].activo){
-			return i;
-		}
-	}
-	return -1;
-}
-
-void insertarPort(grafo pabellon, int pisoDe, int posDe, int pisoA, int posA){
-	pabellon[pisoDe][posDe].insertarArista(pisoA,posA,2);
-	pabellon[pisoA][posA].insertarArista(pisoDe,posDe,2);
-	int cercanoIzq= masCercanoIzq(pabellon, pisoA, posA);							//Busca puntos en el mismo piso a la Izquierda
-	int cercanoDer= masCercanoDer(pabellon, pisoDe, posDe);							//Busca puntos en el mismo piso a la Derecha
-	if(cercanoIzq!=-1){
-		pabellon[pisoDe][posDe].insertarArista(pisoDe,cercanoIzq,pisoDe-cercanoIzq);
-		pabellon[pisoDe][cercanoIzq].insertarArista(pisoDe,posDe,pisoDe-cercanoIzq);
-	}
-	if(cercanoDer!=-1){
-		pabellon[pisoDe][posDe].insertarArista(pisoDe,cercanoDer,cercanoDer-pisoDe);
-		pabellon[pisoDe][cercanoDer].insertarArista(pisoDe,posDe,cercanoDer-pisoDe);
-	}
-}
+#include "ej2.h"
 
 void ejercicio2(string input, string fileOut){
-	grafo pabellon;
 	string line;
 	ifstream fileInput(input.c_str());
+    fileInput.seekg (0, fileInput.end);
+    int length = fileInput.tellg();
+    fileInput.seekg (0, fileInput.beg);
 	getline(fileInput,line);
 	istringstream iss(line);
 	int pisos, L;
 	iss >> pisos >> L;
-	pabellon.resize(pisos);
-	for(int i=0; i<L; i++){
-		pabellon[i].resize(L);
-	}
+
+	grafo pabellon;
+
 	int pisoDe,posDe,pisoA,posA;
+
+	int P = (length-3)/8;								//Magia
+
+	pabellon.rgrafo(pisos,L,P);
+
 	while(getline(fileInput, line, ';')){
 		istringstream iss(line);
 		iss >> pisoDe >> posDe >> pisoA >> posA;
-		insertarPort(pabellon, pisoDe, posDe, pisoA, posA);
+		pabellon.insertarPort(pisoDe, posDe, pisoA, posA);
 	}
+	pabellon.printGraf();
 }
 
 int main(int argc, char **argv){
